8300312c9a0f7f07f1a21ab22e7129575a577c6d
CalebAurand <caurand13@gmail.com>
2022-03-28 17:51:10 -0500

    Finished functionality for passing tests for the rock paper scissors project

--- main.js
+++ main.js
@@ -12,12 +12,74 @@ const rl = readline.createInterface({
 });
 
 // the function that will be called by the unit test below
+
+function validateHand(hand){
+  if(hand=='rock'){
+    // console.log('rock is true');      ////**************** */
+    return true;
+  }else if(hand=='paper'){
+    // console.log('paper is true');
+    return true;
+  }else if(hand=='scissors'){
+    // console.log('scissors are true');
+    return true;
+  }else{
+    // console.log("what is true?");
+    return false;
+  }
+};
+
 const rockPaperScissors = (hand1, hand2) => {
 
   // Write code here
   // Use the unit test to see what is expected
+  let trHand1 = hand1.trim();
+  let trHand2 = hand2.trim();
+  let lwHand1 = trHand1.toLowerCase();
+  let lwHand2 = trHand2.toLowerCase();
 
-}
+  if(validateHand(lwHand1)
+  && validateHand(lwHand2)){
+    // console.log("Let them fight.", lwHand1, lwHand2); ///************** */
+   return combatHands(lwHand1, lwHand2);
+  } else {
+    console.log("Please enter a valid hand");
+  };
+
+};
+
+
+
+const combatHands = (hand1, hand2)=>{
+  // if cases for a tie
+  if(hand1 === 'rock' && hand2 === 'rock'){
+    // console.log("It reached here.");  //********************** */
+    return "It's a tie!";
+  }else if(hand1==='paper' && hand2==='paper'){
+    return "It's a tie!";
+  }else if(hand1==='scissors' && hand2==='scissors'){
+    return "It's a tie!";
+  }
+  // if cases for rock win
+  else if(hand1==='rock' && hand2==='scissors'){
+    return "Hand one wins!";
+  }else if(hand1==='scissors' && hand2==='rock'){
+    return "Hand two wins!";
+  }
+  // if cases for paper win
+  else if(hand1==='paper' && hand2==='rock'){
+    return "Hand one wins!";
+  }else if(hand1==='rock' && hand2==='paper'){
+    return "Hand two wins!";
+  }
+
+  // if cases for scissors win
+  else if(hand1==='scissors' && hand2==='paper'){
+    return "Hand one wins!";
+  }else if(hand1==='paper' && hand2==='scissors'){
+    return "Hand two wins!";
+  }else {return "I don't know"};
+};
 
 // the first function called in the program to get an input from the user
 // to run the function use the command: node main.js
